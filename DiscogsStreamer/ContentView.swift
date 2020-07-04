1f630c4f7ee417fe82d505ae4e4312e989f37f65
Spencer Miles <spencermiles@gmail.com>
2020-07-04 13:07:41 -0700

    Initial Commit

---
+++ DiscogsStreamer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiscogsStreamer
+//
+//  Created by Spencer on 7/4/20.
+//  Copyright © 2020 Spencer Miles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
